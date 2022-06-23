bf1130d901e021289a4afbd604b56092c361aea6
Christian Koch // CRSD <47377645+koch-crsd@users.noreply.github.com>
2022-06-23 16:32:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -263,14 +263,19 @@ exports.extract = function (cwd, opts) {
       xfs.unlink(name, function () {
         var srcpath = path.resolve(cwd, header.linkname)
 
-        xfs.link(srcpath, name, function (err) {
-          if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
-            stream = xfs.createReadStream(srcpath)
-            return onfile()
-          }
+        if(srcpath.indexOf('..') == -1) {
+          xfs.link(srcpath, name, function (err) {
+            if (err && err.code === 'EPERM' && opts.hardlinkAsFilesFallback) {
+              stream = xfs.createReadStream(srcpath)
+              return onfile()
+            }
 
-          stat(err)
-        })
+            stat(err)
+          })
+        } else {
+          console.log('skipping bad path', srcpath);
+        }
+        
       })
     }
 
